d887b62777771d01b4f5b17e48c4035abb1e6ba8
0x7266 <rrrr0ffff@gmail.com>
2023-01-04 13:10:26 -0300

    cors and helmet

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
-require('dotenv').config();
-const express = require('express');
-const connect = require('./config/database.js');
-const index = require('./routes/index.js');
+require("dotenv").config();
+const cors = require("cors");
+const helmet = require("helmet");
+const express = require("express");
+const connect = require("./config/database.js");
+const index = require("./routes/index.js");
 const PORT = process.env.PORT || 3334;
 const app = express();
 
 app.use(express.json());
-app.use('/api', index);
+app.use(cors());
+app.use(helmet());
+app.use("/api", index);
 
 connect(() => {
   app.listen(PORT, () => {
